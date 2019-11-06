bc1f8f984d6e8ae8db26b9efa87d46baa9f71175
nguyenndt278 <53121073+nguyenndt278@users.noreply.github.com>
2019-11-06 10:26:41 -0600

    11/06/19

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,49 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+  var sampledata=`/metadata/${sample}`;
+    d3.json(sampledata).then((sample) => {
+      var samplemeta= d3.select("#sample-metadata");
+      samplemeta.html("");
+      Object.defineProperties(sample).forEach(([key,value]) => {
+        var row= samplemeta.append("p");
+        row.text(`${key}: ${value}`)})
+      });
+    }
 
 function buildCharts(sample) {
-
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  var plotdata= `/samples/${sample}`;
+    d3.json(plotdata).then((data) => {
+      var datafigures= data.sample_values;
+      var colors= data.otu_ids;
+      var trace=[{
+        x: colors,
+        y: datafigures,
+        mode: 'markers',
+        marker: {size: datafigures, color: colors}
+      }];
+      var layout= {
+        title: "Belly Button Bacteria Count",
+        xaxis: {title: "OTU - ID"}
+      };
+      Plotly.newPlot('bubble', trace, layout);
+    d3.json(datafigures).then((data) => {
+      var trace1= {
+        values: data.sample_values.slice(0,10),
+        labels: data.otu_ids,
+        type: "pie",
+        hole: .5
+      };
+      var dataa=[trace1];
+      var layout= {
+        title: "Pie chart"
+      };
+      Plotly.newPlot('pie', dataa, layout);
+    })
+    })
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
       selector
@@ -38,8 +51,6 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
-    // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
@@ -47,10 +58,8 @@ function init() {
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
 
-// Initialize the dashboard
 init();
